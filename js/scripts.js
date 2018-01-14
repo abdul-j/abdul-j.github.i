d3ebc90c5686bde7eaef701ce954a88fa8e44f93
abdul-j <32846580+abdul-j@users.noreply.github.com>
2018-01-14 00:23:03 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
+var button = $('#reveal-button');
 
+var hiddenElement = $('#hideaway');
+
+button.click(function () {
+  if (hiddenElement.attr('style') == 'display:none;') {
+    hiddenElement.attr('style', 'display:block;');
+    button.text('HIDE');
+  }
+  else {
+    hiddenElement.attr('style', 'display:none;');
+    button.text('REVEAL');
+  }
+});
